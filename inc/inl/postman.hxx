7f5d3a0730ddd557c6d65d6ef8ae38c3ea4c2fc8
Pato <miti145@gmail.com>
2023-08-22 14:07:20 +0300

    postman: fixed hxx file to new template definition

--- inc/inl/postman.hxx
+++ inc/inl/postman.hxx
@@ -2,15 +2,15 @@
 
 namespace fgear {
 
-template<typename K, typename V>
-Postman<K, V>::Postman(concurrency::BlockingMap<K, V> const& a_map, std::shared_ptr<Observer<K>> const& a_observer)
+template<typename V>
+Postman<V>::Postman(std::shared_ptr<concurrency::BlockingMap<std::string, V>> const& a_map, std::shared_ptr<Observer> const& a_observer)
 : m_map{a_map}
 , m_observer{a_observer}
 {
 }
 
-template<typename K, typename V>
-void Postman<K, V>::update(K const& a_key, V const& a_value)
+template<typename V>
+void Postman<V>::update(std::string const& a_key, V const& a_value)
 {
     m_map[a_key] = a_value;
     m_observer.get().notify(a_key);
