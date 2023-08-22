2657178fa9daf751d70fd709bcfd34a5f4c92573
Pato <miti145@gmail.com>
2023-08-22 13:03:09 +0300

    created postman class hpp file

---
+++ inc/postman.hpp
@@ -0,0 +1,35 @@
+#ifndef POSTMAN_HPP
+#define POSTMAN_HPP
+//for server
+
+#include "blocking_map.hpp"
+#include "observer.hpp"
+//TODO: manager class
+
+#include <string> // std::string
+#include <memory> // shared_ptr
+#include <mutex> // mutex
+
+//TODO: enum class
+//TODO: read map from file
+
+namespace fgear {
+
+template<typename K, typename V>
+class Postman {
+public:
+    Postman(concurrency::BlockingMap<K, V> const& a_map, std::shared_ptr<Observer>& a_observer);
+
+    void update(K const& a_key, V const& a_value);    
+
+private:
+    concurrency::BlockingMap<K, V> m_map;
+    std::shared_ptr<Observer<K>> m_observer;
+    std::mutex m_mutex;
+};
+
+} // namespace fgear
+
+#include "inl/postman.hxx"
+
+#endif // POSTMAN_HPP
